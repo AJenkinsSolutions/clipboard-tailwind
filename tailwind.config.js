d47117fe4dd69a2e38e3bb52d0e2a71fb2995d94
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-04-10 12:39:12 -0500

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      //CustomColors using hsl
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(223, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'], 
+      }
+    },
+  },
+  plugins: [],
+}
+
